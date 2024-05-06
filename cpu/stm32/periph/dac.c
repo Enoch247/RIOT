b70182250fdb439dc37e9241bb8dd52c96b31667
Joshua DeWeese <jdeweese@primecontrols.com>
2024-05-06 11:34:39 -0400

    WIP

--- cpu/stm32/periph/dac.c
+++ cpu/stm32/periph/dac.c
@@ -43,6 +43,8 @@
 #define RCC_BIT             (RCC_APB1ENR_DAC1EN)
 #elif defined(RCC_APB1ENR1_DAC1EN)
 #define RCC_BIT             (RCC_APB1ENR1_DAC1EN)
+#elif defined(RCC_APB1LENR_DAC12EN)
+#define RCC_BIT             (RCC_APB1LENR_DAC12EN)
 #else
 #define RCC_BIT             (RCC_APB1ENR_DACEN)
 #endif
